03f7109ab53101af7c2e80ad0ddc0db15315b4e7
Edson Segura <edsonsegurapesantes@gmail.com>
2026-07-21 16:41:41 -0500

    fix: puerto dinamico para render

--- main.ts
+++ main.ts
@@ -19,6 +19,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  // Puerto dinámico para Render (usa process.env.PORT o el 3000 por defecto localmente)
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
 }
 bootstrap();
